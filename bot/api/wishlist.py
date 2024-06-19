97d9913a7b9990498b30217a14de4d93cf592bed
naftalino <jntslinux@gmail.com>
2024-06-19 20:44:53 -0300

    modified:   bot/api/wishlist.py 	modified:   bot/commands/wishlist.py

--- bot/api/wishlist.py
+++ bot/api/wishlist.py
@@ -1,14 +1,21 @@
 import httpx
 
 class Wishlist:
-    def buscar(user_id):
-        return True
+    def criar_wishlist(user_id, nome):
+        header = {"Content-type":"application/json"}
+        js = {"nome":str(nome), "user_id":int(user_id)}
+        r = httpx.post("http://localhost:3000/cadastrar/wl/", headers=header, json=js).json()
+        return r['retorno']
 
-    def inserir_item_wishlist(user_id, carta_id):
-        return True
-    
+    def inserir_item_wishlist(user_id, wishlist_id, carta_id=1):
+        r = httpx.get(f"http://localhost:3000/inserir/wl/{wishlist_id}/{user_id}/{carta_id}").json()
+        print(r)
+
     def remover_item_wishlist(user_id, carta_id):
         return True
 
     def wishlist_completa(user_id, lista_id):
+        return True
+
+    def wishlists_usuario(user_id):
         return True
--- bot/commands/wishlist.py
+++ bot/commands/wishlist.py
@@ -1,18 +1,16 @@
 from telegram.ext import Updater, ContextTypes
 from api.conta import Conta
 from api.carta import Carta
+from api.wishlist import *
 
 async def criar_wl(update: Updater, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
     n = update.message.from_user.first_name
-    texto, link  = info_conta(user_id, n)
 
 async def buscar_wl(update: Updater, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
     n = update.message.from_user.first_name
-    texto, link  = info_conta(user_id, n)
 
 async def listar_wl(update: Updater, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
     n = update.message.from_user.first_name
-    texto, link  = info_conta(user_id, n)
