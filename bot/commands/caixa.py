d21304893969441a19c203f69b826310e8af330f
naftalino <jntslinux@gmail.com>
2024-06-25 22:43:56 -0300

    modifiquei umas coisa aekkkk

--- bot/commands/caixa.py
+++ bot/commands/caixa.py
@@ -1,6 +1,7 @@
 from api.conta import Conta
 from api.obra import Obra
 from telegram.ext import Updater, ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 # 4. Loja
 # 4.1 A partir do comando /caixa ele escolhe entre os seguintes botões*:
@@ -42,17 +43,44 @@ from telegram.ext import Updater, ContextTypes
 
 # retorna a mensagem de loja
 async def atendente(update: Updater, context: ContextTypes.DEFAULT_TYPE):
-    return
+    mensagem = """
+Opa! Aqui é o caixa!
+
+O que gostaria de realizar?
+    """
+
+    botao = [
+    [
+        InlineKeyboardButton("🔄 Devolução", callback_data="iniciar_devolucao")
+    ],
+    [
+        InlineKeyboardButton("🛍 Adquirir pedidos", callback_data="adquirir_pedidos")
+    ],
+    [
+        InlineKeyboardButton("🎁 Presentear", callback_data="presentear")
+    ],
+    [
+        InlineKeyboardButton("🫙 Ingredientes na vitrine", callback_data="ingredientes_vitrine")
+    ]
+    ]
+
+    teclado = InlineKeyboardMarkup(botao)
+    photo = "https://i.pinimg.com/originals/f2/82/08/f28208cdccf656bd4b800d065863c7e3.jpg"
+    await update.message.reply_photo(photo, caption=mensagem, reply_markup=teclado)
 
 # pergunta quais cartas quer remover
 async def iniciar_devolucao(update: Updater, context: ContextTypes.DEFAULT_TYPE):
+    print("Blz pae, quais cartas?")
     return
 
 # confirma se as cartas sao da pessoa
 # manda uma msg de confirmacao para remover
 async def confirmar_devolucao(update: Updater, context: ContextTypes.DEFAULT_TYPE):
+    # sim -> print("blz cara mas nao tem uma carta ai que nao ta na tua conta nao kkk melhore pf")
+    # nao -> print("blz, bora, la, so confirma para mim se ta tudo ok.")
     return
 
 # passando do outro, ele remove e da as moedas, enviando quantas ganhou e quantas tem agora
 async def devolver(update: Updater, context: ContextTypes.DEFAULT_TYPE):
+    print("nao cara, pode crer, vou remover aqui e te dou essas migalhas aq pdp?")
     return
